6fd40061047724bebc37359f30a6acf0db8607fa
Chad <657855247@qq.com>
2021-01-20 12:09:31 +0800

    Remove useless code.

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -4,7 +4,6 @@ import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import androidx.lifecycle.lifecycleScope
 import androidx.room.Room
 import com.example.myapplication.databinding.ActivityMainBinding
